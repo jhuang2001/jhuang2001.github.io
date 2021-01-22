b2b86e38942016c703caf31e021478b2c4cf114e
Jeren Huang <jhuang00021@gmail.com>
2021-01-21 21:23:21 -0800

    added basic mobile support

--- app.js
+++ app.js
@@ -7,49 +7,58 @@ const description = document.querySelector('.summary');
 //constants
 const cardRect = card.getBoundingClientRect();
 
-//check if mobile
-function isMobileDevice() {
-    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+var cardRotate = function (e) {
+    var x, y;
+
+    if(e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel'){
+        var evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent;
+        var touch = evt.touches[0] || evt.changedTouches[0];
+        x = touch.pageX;
+        y = touch.pageY;
+    } else if (e.type == 'mousedown' || e.type == 'mouseup' || e.type == 'mousemove' || e.type == 'mouseover'|| e.type=='mouseout' || e.type=='mouseenter' || e.type=='mouseleave') {
+        x = e.clientX;
+        y = e.clientY;
+    }
+
+    card.style.transition = "none";
+    let defaultTurn = 0.04 * (window.innerWidth / 2 - cardRect.width / 2 - cardRect.left);
+    let yTurn = (x - window.scrollX - cardRect.width / 2) / (window.innerWidth / 25) + defaultTurn;
+    let xTurn = (y - window.scrollY - cardRect.height / 2) / (window.innerHeight / 30);
+    card.style.transform = `rotateY(${yTurn}deg) rotateX(${-xTurn}deg)`
+    console.log(x);
 };
 
+var cardResetRotation = function (e){
+    console.log("reset");
+    card.style.transition = "all 0.75s ease "
+    //myName.style.transform = "translateZ(50px)"
+    let defaultTurn = 0.04 * (window.innerWidth / 2 - cardRect.width / 2 - cardRect.left);
+    card.style.transform = `rotateY(${defaultTurn}deg) rotateX(0deg)`;
+};
 
-if(!isMobileDevice()){
-    container.addEventListener("mousemove", (e) => {
-        card.style.transition = "none";
+container.addEventListener("mousemove", cardRotate, false);
+container.addEventListener("touchmove", cardRotate, false);
 
-        let defaultTurn = 0.04 * (window.innerWidth/2 - cardRect.width/2 - cardRect.left);
-        let yTurn = (e.pageX-window.scrollX - cardRect.width/2) / (window.innerWidth/25) + defaultTurn;
-        let xTurn = (e.pageY-window.scrollY - cardRect.height/2) / (window.innerHeight/30);
-        card.style.transform = `rotateY(${yTurn}deg) rotateX(${-xTurn}deg)`
-        console.log(e.pageY);
-    });
+card.addEventListener("mouseenter", (e) => {
+    //console.log("TEST");
+    card.style.transition = "none";
+    profile.style.transform = "translateZ(250px) rotateZ(12deg)"
+    myName.style.transform = "translateZ(300px)"
+    description.style.transform = "translateZ(150px)"
+});
 
-    card.addEventListener("mouseenter", (e) => {
-        console.log("TEST");
-        card.style.transition = "none";
-        profile.style.transform = "translateZ(250px) rotateZ(12deg)"
-        myName.style.transform = "translateZ(300px)"
-        description.style.transform = "translateZ(150px)"
-    });
+card.addEventListener("mouseleave", (e) => {
+    profile.style.transform = "translateZ(5px) rotateZ(0deg)"
+    myName.style.transform = "translateZ(50px)"
+    description.style.transform = "translateZ(0px)"
+});
 
-    card.addEventListener("mouseleave", (e) => {
-        profile.style.transform = "translateZ(5px) rotateZ(0deg)"
-        myName.style.transform = "translateZ(50px)"
-        description.style.transform = "translateZ(0px)"
-    });
+window.addEventListener("mouseout", cardResetRotation, false);
+window.addEventListener("touchend", cardResetRotation, false);
+window.addEventListener("touchcancel", cardResetRotation, false);
 
-    window.addEventListener("mouseout", (e) => {
-        card.style.transition = "all 0.75s ease "
-        //myName.style.transform = "translateZ(50px)"
-        let defaultTurn = 0.04 * (window.innerWidth/2 - cardRect.width/2 - cardRect.left);
-        card.style.transform = `rotateY(${defaultTurn}deg) rotateX(0deg)`
-    });
 
-    document.addEventListener('DOMContentLoaded', function() {
-        profile.style.transform = "translateZ(5px) rotateZ(0deg)"
-        myName.style.transform = "translateZ(50px)"
-    }, false);
-}
-else{
-    
-}
+document.addEventListener('DOMContentLoaded', function () {
+    profile.style.transform = "translateZ(5px) rotateZ(0deg)"
+    myName.style.transform = "translateZ(50px)"
+}, false);
